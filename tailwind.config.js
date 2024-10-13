3f1d969866ff5ba2f98b9fad61bd8f5d3298ad38
Innovative-J <juan.cia05678@gmail.com>
2024-10-13 11:39:47 -0500

    add more pages, pathing and deleted unused files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,41 @@
+const { DEFAULT_RUNTIME_WEBPACK } = require('next/dist/shared/lib/constants');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  prefix: "",
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+	container: {
+		center: true,
+		padding: "15px",
+		screens: {
+			sm: "640px",
+			md: "768px",
+			lg: "960px",
+			xl: "1200px",
+		},
+	},
+	fontFamily: {
+		primary: "var(--font-playfair)",
+	},
+  	extend: {
+  		colors: {
+			// maybe reconsider changing the colors 
+			// textColor: "#B9C1C4",
+			primary: "#9fb1bcff",
+			accent: {
+				// default is the hover thats already clicked
+				DEFAULT: "#344850",
+				hover: "#2e5266ff"
+				// Hex codes: Sapphire (#2E5266FF), Light Slate Gray (#6E8898FF), Cadet Gray (#9FB1BCFF), and American Silver (#D3D0CBFF)
+			},
+  		},
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
